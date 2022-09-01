ad50aaeee08cfc0228de35b945ae84d8153776d6
Brandon <b.pintony@gmail.com>
2022-09-01 09:41:07 -0400

    commit tree a4d1d2184205aa3be7d86c8c520c9c8aee6af621

--- enumerables.rb
+++ enumerables.rb
@@ -3,16 +3,15 @@ require 'pry'
 # this method returns an array of hashes, which we'll use in the other methods
 def spicy_foods 
   [
-    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
-    { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
-    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 }
+    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 }.stringify_keys,
+    { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 }.stringify_keys,
+    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 }.stringify_keys
   ]
 end
 
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
 end
 
 # given an array of spicy foods, **return an array of hashes** 
